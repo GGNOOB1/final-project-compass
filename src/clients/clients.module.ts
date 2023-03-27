6c6d8d2b91794dd4cf167c2c476597d373e77cd5
Geraldo Vicente <geraldovicente987@gmail.com>
2023-03-27 12:24:36 -0300

    feat: Added clients and cars module

--- src/clients/clients.module.ts
+++ src/clients/clients.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ClientsController } from './clients.controller';
 import { ClientsService } from './clients.service';
+import { CarsModule } from './cars/cars.module';
 
 @Module({
   controllers: [ClientsController],
-  providers: [ClientsService]
+  providers: [ClientsService],
+  imports: [CarsModule]
 })
 export class ClientsModule {}
