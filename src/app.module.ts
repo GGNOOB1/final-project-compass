2188c4c525aa077ab7fbd0a8c302a4aa5c49ee87
Geraldo Vicente <geraldovicente987@gmail.com>
2023-03-27 10:54:54 -0300

    build: Initializing the nest cli

---
+++ src/app.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ClientsModule } from './clients/clients.module';
+import { MechanicsModule } from './mechanics/mechanics.module';
+import { PartsModule } from './parts/parts.module';
+import { AuthModule } from './auth/auth.module';
+import { ServicesModule } from './services/services.module';
+
+@Module({
+  imports: [ClientsModule, MechanicsModule, PartsModule, AuthModule, ServicesModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
