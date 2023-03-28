4e7a48825963b6fd8a6314527b102c71e20e44cc
Geraldo Vicente <geraldovicente987@gmail.com>
2023-03-28 13:25:49 -0300

    feat: Added database configuration, typeorm and environment variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,28 @@ import { MechanicsModule } from './mechanics/mechanics.module';
 import { PartsModule } from './parts/parts.module';
 import { AuthModule } from './auth/auth.module';
 import { ServicesModule } from './services/services.module';
+import { TypeOrmModule } from '@nestjs/typeorm/dist/typeorm.module';
+import { ConfigModule } from '@nestjs/config';
+import { Clients } from './clients/clients.entity';
+import { Cars } from './clients/cars/cars.entity';
+import { Mechanics } from './mechanics/mechanics.entity';
+import { Parts } from './parts/parts.entity';
+import { Services } from './services/services.entity';
+import { Specialties } from './mechanics/specialties.entity';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      database: process.env.DB_NAME,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      port: Number(process.env.DB_PORT),
+      synchronize: true,
+      entities: [Clients, Cars, Mechanics, Parts, Services, Specialties],
+    }),
     ClientsModule,
     MechanicsModule,
     PartsModule,
