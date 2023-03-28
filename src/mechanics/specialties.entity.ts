c3f002710e94f22f8d816fcb211ca84cc2a55e80
Geraldo Vicente <geraldovicente987@gmail.com>
2023-03-28 13:26:27 -0300

    feat: Added new entity of specialties

---
+++ src/mechanics/specialties.entity.ts
@@ -0,0 +1,16 @@
+import { Entity, ManyToOne } from 'typeorm';
+import { Column } from 'typeorm/decorator/columns/Column';
+import { PrimaryGeneratedColumn } from 'typeorm/decorator/columns/PrimaryGeneratedColumn';
+import { Mechanics } from './mechanics.entity';
+
+@Entity()
+export class Specialties {
+  @PrimaryGeneratedColumn()
+  id: string;
+
+  @Column()
+  name: string;
+
+  @ManyToOne(() => Mechanics, (mechanics) => mechanics.specialties)
+  specialty: Specialties;
+}
