c7e15c2139e2bd851c9d1259f97ef457d1ff361f
Geraldo Vicente <geraldovicente987@gmail.com>
2023-03-30 10:49:20 -0300

    feat: Utility function that removes specific fields from the response

---
+++ src/utils/removeFields.ts
@@ -0,0 +1,13 @@
+export const removeFields = (
+  object: object | object[],
+  fields: string[],
+): object => {
+  try {
+    fields.forEach((field) => delete object[field]);
+    return object;
+  } catch (e) {
+    return {
+      message: e.message,
+    };
+  }
+};
