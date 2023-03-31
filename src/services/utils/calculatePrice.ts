3f00efcd773a07f79cde85c0daa3a926d4f90c73
Geraldo Vicente <geraldovicente987@gmail.com>
2023-03-31 14:25:38 -0300

    feat: Calculates the total price of services

---
+++ src/services/utils/calculatePrice.ts
@@ -0,0 +1,16 @@
+export function calculatePrice(orderPart, currentPart, serviceFeeValue) {
+  const orderQtd = orderPart.qtd;
+  const currentQtd = currentPart.qtd;
+  const quantityLeft = currentQtd - orderQtd;
+  const priceProduct = parseFloat(currentPart.unitPrice);
+  const serviceFee = serviceFeeValue;
+  const totalPriceWithoutFee = orderQtd * priceProduct;
+  const totalPriceRight =
+    totalPriceWithoutFee - totalPriceWithoutFee * (serviceFee / 100);
+
+  return {
+    totalPriceRight,
+    quantityLeft,
+    orderQtd,
+  };
+}
