d2e55bb1768228a64285c10c9fdd8a6ee1991d80
Geraldo Vicente <geraldovicente987@gmail.com>
2023-03-31 14:19:20 -0300

    refactor: Creating 2 separate auth services instead of 1 and implementing login logic

---
+++ src/auth/auth.clients.service.ts
@@ -0,0 +1,78 @@
+import {
+  Injectable,
+  NotFoundException,
+  BadRequestException,
+} from '@nestjs/common';
+import { ClientsService } from 'src/clients/clients.service';
+
+import { LoginUpdatePasswordDto } from './dtos/login-Updatepassword.dto';
+import { JwtService } from '@nestjs/jwt';
+import { verifyPasswordAndEmail } from './utils/verifyPasswordAndEmail';
+import { comparePasswords } from './utils/comparePasswords';
+import { verifyEmail } from './utils/verifyEmail';
+import { encryptPassword } from 'src/utils/encryptPassword';
+
+@Injectable()
+export class AuthClientService {
+  constructor(
+    private clientsService: ClientsService,
+    private jwtService: JwtService,
+  ) {}
+
+  async verifyPasswordAndEmail(loginClient: LoginUpdatePasswordDto) {
+    const user = await this.clientsService.findOneByEmail(loginClient.email);
+
+    if (!user) {
+      throw new NotFoundException('This email does not exist');
+    }
+    const hashedPassword = user['password'];
+
+    if (!(await comparePasswords(loginClient.password, hashedPassword))) {
+      throw new BadRequestException('Your password is wrong!');
+    }
+
+    return user;
+  }
+
+  async signIn(loginClient: LoginUpdatePasswordDto) {
+    try {
+      const user = await verifyPasswordAndEmail(
+        this.clientsService,
+        comparePasswords,
+        loginClient,
+      );
+      const payload = { username: user['name'], sub: user['id'] };
+      return {
+        access_token: await this.jwtService.signAsync(payload),
+      };
+    } catch (e) {
+      return {
+        message: e.message,
+      };
+    }
+  }
+
+  async updatePassword(loginClient: LoginUpdatePasswordDto) {
+    const user = await verifyEmail(this.clientsService, loginClient);
+
+    loginClient.password = await encryptPassword(loginClient.password);
+
+    await this.clientsService.updatePassword(user['id'], loginClient.password);
+
+    return;
+  }
+
+  async refreshToken(token: string) {
+    const verificatedToken = await this.jwtService.verifyAsync(token);
+    const user = await this.clientsService.findById(verificatedToken.sub);
+    const payload = {
+      username: user['name'],
+      sub: user['id'],
+    };
+
+    const newToken = await this.jwtService.signAsync(payload);
+    return {
+      newToken,
+    };
+  }
+}
---
+++ src/auth/auth.mechanics.service.ts
@@ -0,0 +1,75 @@
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { MechanicsService } from 'src/mechanics/mechanics.service';
+import * as bcrypt from 'bcrypt';
+import { LoginUpdatePasswordDto } from './dtos/login-Updatepassword.dto';
+import { JwtService } from '@nestjs/jwt/dist';
+import { verifyPasswordAndEmail } from './utils/verifyPasswordAndEmail';
+import { comparePasswords } from './utils/comparePasswords';
+import { encryptPassword } from 'src/utils/encryptPassword';
+import { verifyEmail } from './utils/verifyEmail';
+
+@Injectable()
+export class AuthMechanicsService {
+  constructor(
+    private mechanicsService: MechanicsService,
+    private jwtService: JwtService,
+  ) {}
+
+  async signIn(loginMechanic: LoginUpdatePasswordDto) {
+    try {
+      const user = await verifyPasswordAndEmail(
+        this.mechanicsService,
+        comparePasswords,
+        loginMechanic,
+      );
+
+      if (!user) {
+        throw new NotFoundException('This email does not exist');
+      }
+      const hashedPassword = user['password'];
+
+      if (!(await comparePasswords(loginMechanic.password, hashedPassword))) {
+        throw new BadRequestException('Your password is wrong!');
+      }
+      const payload = { username: user['name'], sub: user['id'] };
+      return {
+        access_token: await this.jwtService.signAsync(payload),
+      };
+    } catch (e) {
+      return {
+        message: e.message,
+      };
+    }
+  }
+
+  async updatePassword(loginMechanic: LoginUpdatePasswordDto) {
+    const user = await verifyEmail(this.mechanicsService, loginMechanic);
+
+    loginMechanic.password = await encryptPassword(loginMechanic.password);
+
+    await this.mechanicsService.updatePassword(
+      user['id'],
+      loginMechanic.password,
+    );
+
+    return;
+  }
+
+  async refreshToken(token: string) {
+    const verificatedToken = await this.jwtService.verifyAsync(token);
+    const user = await this.mechanicsService.findById(verificatedToken.sub);
+    const payload = {
+      username: user['name'],
+      sub: user['id'],
+    };
+
+    const newToken = await this.jwtService.signAsync(payload);
+    return {
+      newToken,
+    };
+  }
+}
