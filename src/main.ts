eb020b2acf9855d8e6cbf32c05b743e2baa292d9
Geraldo Vicente <geraldovicente987@gmail.com>
2023-03-31 16:32:06 -0300

    Added filters to main file for execution

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DatabaseExceptionFilter } from './filters/dbExceptionFilter';
+import { ValidationExceptionFilter } from './filters/validationExceptionFilter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,6 +12,9 @@ async function bootstrap() {
       whitelist: true,
     }),
   );
+
+  app.useGlobalFilters(new DatabaseExceptionFilter());
+  app.useGlobalFilters(new ValidationExceptionFilter());
 
   await app.listen(3000);
 }
