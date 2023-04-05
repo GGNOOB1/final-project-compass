18d969a3109ec78cdd8456ef26b9fa40791fc319
Geraldo Vicente <geraldovicente987@gmail.com>
2023-04-05 14:10:52 -0300

    feat: Interceptor that handles response date data

---
+++ src/interceptors/date-format.interceptor.ts
@@ -0,0 +1,43 @@
+import {
+  Injectable,
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler,
+} from '@nestjs/common';
+
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+import { formatDateForResponse } from 'src/utils/formatDateForResponse';
+
+@Injectable()
+export class DateFormatInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    return next.handle().pipe(
+      map((data) => {
+        if (data.items) {
+          data.items.map((item) => this.formatDate(item));
+          return data;
+        } else {
+          return this.formatDate(data);
+        }
+      }),
+    );
+  }
+
+  private formatDate(data: any): any {
+    if (data.birthday) {
+      data.birthday = formatDateForResponse(data.birthday);
+    }
+    if (data.hiringDate) {
+      data.hiringDate = formatDateForResponse(data.hiringDate);
+    }
+
+    if (data.serviceEstimatedDeliveryDate) {
+      data.serviceEstimatedDeliveryDate = formatDateForResponse(
+        data.serviceEstimatedDeliveryDate,
+      );
+    }
+    return data;
+  }
+}
