5b1246ff37689cff05250ed9be90131708d49bfe
Geraldo Vicente <geraldovicente987@gmail.com>
2023-04-05 14:09:49 -0300

    refactor:  Adding validator to format dates in mm/dd/yyyy format

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -1,4 +1,13 @@
+import { BadRequestException } from '@nestjs/common';
+import * as moment from 'moment';
+
 export function formatDate(date) {
+  if (!moment(date, 'DD/MM/YYYY', true).isValid()) {
+    throw new BadRequestException(
+      'Your date is outside the standard format: DD/MM/YYYY',
+    );
+  }
+
   const currentDate = String(date);
   const formatedDate = currentDate.split('/').reverse().join('-');
 
