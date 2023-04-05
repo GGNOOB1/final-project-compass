5b6c0e9af9a056ba93e8d8427d3fae2f3283b42c
Geraldo Vicente <geraldovicente987@gmail.com>
2023-04-05 10:37:45 -0300

    refactor: Added returning type

--- src/auth/utils/verifyEmail.ts
+++ src/auth/utils/verifyEmail.ts
@@ -1,6 +1,11 @@
 import { NotFoundException } from '@nestjs/common';
+import { Clients } from 'src/clients/clients.entity';
+import { Mechanics } from 'src/mechanics/mechanics.entity';
 
-export async function verifyEmail(service, login) {
+export async function verifyEmail(
+  service,
+  login,
+): Promise<Clients | Mechanics> {
   const user = await service.findOneByEmail(login.email);
 
   if (!user) {
