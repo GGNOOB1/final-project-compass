4b3e8aa85a8aa5ccf9aaf72ea398c757b5edf693
Geraldo Vicente <geraldovicente987@gmail.com>
2023-04-05 14:16:15 -0300

    refactor: Added interceptor data validator to globalInterceptor

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DatabaseExceptionFilter } from './filters/dbExceptionFilter';
 import { ValidationExceptionFilter } from './filters/validationExceptionFilter';
+import { DateFormatInterceptor } from './interceptors/date-format.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,7 +13,7 @@ async function bootstrap() {
       whitelist: true,
     }),
   );
-
+  app.useGlobalInterceptors(new DateFormatInterceptor());
   app.useGlobalFilters(new DatabaseExceptionFilter());
   app.useGlobalFilters(new ValidationExceptionFilter());
 
