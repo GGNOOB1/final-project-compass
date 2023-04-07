32f1f4d081c06e65476e9f3fb8d9fb17f6186798
Geraldo Vicente <geraldovicente987@gmail.com>
2023-04-07 12:14:56 -0300

    refactor: Removing global data interceptor and manually placing it in controllers

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DatabaseExceptionFilter } from './filters/dbExceptionFilter';
 import { ValidationExceptionFilter } from './filters/validationExceptionFilter';
-import { DateFormatInterceptor } from './interceptors/date-format.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,7 +12,6 @@ async function bootstrap() {
       whitelist: true,
     }),
   );
-  app.useGlobalInterceptors(new DateFormatInterceptor());
   app.useGlobalFilters(new DatabaseExceptionFilter());
   app.useGlobalFilters(new ValidationExceptionFilter());
 
