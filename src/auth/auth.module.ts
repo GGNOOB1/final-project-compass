1007a3e002728ea1532831661b2935fb965de3ef
Geraldo Vicente <geraldovicente987@gmail.com>
2023-04-09 17:35:51 -0300

    refactor: small modification

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -10,7 +10,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Mechanics } from '../mechanics/mechanics.entity';
 import { Specialties } from '../mechanics/specialties.entity';
 import { Clients } from '../clients/clients.entity';
-import { JwtModule } from '@nestjs/jwt/dist';
+import { JwtModule, JwtService } from '@nestjs/jwt/dist';
 import { ConfigModule } from '@nestjs/config';
 import { AuthMechanicsService } from './auth.mechanics.service';
 
