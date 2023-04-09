f14b00aeb8f81284eb64b7302d0c306243828e41
Geraldo Vicente <geraldovicente987@gmail.com>
2023-04-09 17:35:31 -0300

    refactor: Added new ways to authenticate tokens

--- src/interceptors/auth.interceptor.ts
+++ src/interceptors/auth.interceptor.ts
@@ -4,24 +4,62 @@ import {
   ExecutionContext,
   CallHandler,
   UnauthorizedException,
+  NotFoundException,
 } from '@nestjs/common';
 import { Observable } from 'rxjs';
+import { ClientsService } from '../clients/clients.service';
+import { MechanicsService } from '../mechanics/mechanics.service';
 
 @Injectable()
 export class AuthInterceptor implements NestInterceptor {
-  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+  constructor(
+    private clientsService: ClientsService,
+    private mechanicsService: MechanicsService,
+  ) {}
+
+  async intercept(
+    context: ExecutionContext,
+    next: CallHandler,
+  ): Promise<Observable<any>> {
     const req = context.switchToHttp().getRequest();
 
     if (req['user'].type === 'client' && req.route.path.includes('mechanic')) {
       throw new UnauthorizedException(
-        'Something went wrong, the token you are logged in with is not the same as the resource you want to access',
+        'Something went wrong, This route is only accessible to mechanics',
       );
     }
 
     if (req['user'].type === 'mechanic' && req.route.path.includes('client')) {
       throw new UnauthorizedException(
-        'Something went wrong, the token you are logged in with is not the same as the resource you want to access',
+        'Something went wrong, This route is only accessible to clients',
       );
+    }
+
+    if (req['user'].type === 'mechanic') {
+      const user = await this.mechanicsService.findOneByEmail(req.body.email);
+
+      if (!user) {
+        throw new NotFoundException('This mechanic email does not exist');
+      }
+
+      if (user.id !== req['user'].sub) {
+        throw new UnauthorizedException(
+          'The mechanic id you are trying to access is not yours',
+        );
+      }
+    }
+
+    if (req['user'].type === 'client') {
+      const user = await this.clientsService.findOneByEmail(req.body.email);
+
+      if (!user) {
+        throw new NotFoundException('This client email does not exist');
+      }
+      if (user.id !== req['user'].sub) {
+        throw new UnauthorizedException(
+          'The client id you are trying to access is not yours',
+        );
+      }
     }
 
     return next.handle();
