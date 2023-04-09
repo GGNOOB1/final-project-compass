8abf8fc80cf131d4228db7f6ab2b5c56238461fa
Geraldo Vicente <geraldovicente987@gmail.com>
2023-04-09 17:34:21 -0300

    refactor: Added message in return error

--- src/auth/guards/jwt.guard.ts
+++ src/auth/guards/jwt.guard.ts
@@ -15,7 +15,9 @@ export class JwtAuth implements CanActivate {
     const request = context.switchToHttp().getRequest();
     const token = this.extractTokenFromHeader(request);
     if (!token) {
-      throw new UnauthorizedException();
+      throw new UnauthorizedException(
+        'The token you received when logging in to the api is required',
+      );
     }
     try {
       const payload = await this.jwtService.verifyAsync(token, {
