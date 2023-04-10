39c7ab0d69b5122e0e129a4155d1c67934f95b1f
Geraldo Vicente <geraldovicente987@gmail.com>
2023-04-10 08:43:45 -0300

    refactor: Added swagger documentation

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import { DatabaseExceptionFilter } from './filters/dbExceptionFilter';
+import { DatabaseExceptionFilter } from './filters/dbException.filter';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Auto Car API')
+    .setDescription(
+      'Last challenge implementing all the technologies studied during the internship. ' +
+        'Creating an auto car system to manage customers, mechanics, services, cars and parts.',
+    )
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
 
   app.useGlobalPipes(
     new ValidationPipe({
